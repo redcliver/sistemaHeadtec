a01dd0c96b8a1cefd8f05e8e29a8671a4388a4f0
redcliver <igor-peres@hotmail.com>
2020-01-05 18:58:46 -0200

    1.0
    base gerencia icones

--- sistemaHT/settings.py
+++ sistemaHT/settings.py
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'sistemaHT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sistemaHT',
-        'USER': 'postgres',
-        'PASSWORD': '957855',
-        'HOST': '127.0.0.1',
-        'PORT': '', # 8000 is default
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'sistemaHT',
+#        'USER': 'postgres',
+#        'PASSWORD': '957855',
+#        'HOST': '127.0.0.1',
+#        'PORT': '', # 8000 is default
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
